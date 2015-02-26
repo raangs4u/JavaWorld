5b47f585b66eb1aeca285626134c95f3f1e8f38a
Ranga Babu <raangs4u@gmail.com>
2015-02-27 01:53:19 +0530

    Random Contraction algorithm

--- src/main/java/com/hr/Graph.java
+++ src/main/java/com/hr/Graph.java
@@ -1,9 +1,6 @@
 package com.hr;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by RANGA on 2/23/2015.
@@ -13,12 +10,23 @@ public class Graph {
     private int V;
 
     private int E;
-    private Map<Integer,List> adjListMap;
+
+    public Map<Integer, List<Integer>> getAdjListMap() {
+        return adjListMap;
+    }
+
+    private Map<Integer,List<Integer>> adjListMap;
+
+    public Set getVerticesSet() {
+        return verticesSet;
+    }
+
+    private Set<Integer> verticesSet = new HashSet<Integer>();
 
     public Graph(int size) {
         V = size;
         E = 0;
-        adjListMap = new HashMap<Integer, List>(size);
+        adjListMap = new HashMap<Integer, List<Integer>>(size);
     }
 
     public int getV() {
@@ -38,24 +46,52 @@ public class Graph {
     }
 
     public void addEdge(int v, int u) {
-        List list = adjListMap.get(v);
-        if(list == null){
-            list = new ArrayList();
-            list.add(u);
-            adjListMap.put(v,list);
-
+        verticesSet.add(v);
+        verticesSet.add(u);
+        List listv = adjListMap.get(v);
+        if(listv == null){
+            listv = new ArrayList<Integer>();
+            listv.add(u);
+            adjListMap.put(v,listv);
+            verticesSet.add(v);
         } else {
-            list.add(u);
+            listv.add(u);
+        }
+        List listu =adjListMap.get(u);
+        if(listu == null){
+            listu = new ArrayList<Integer>();
+            listu.add(v);
+            adjListMap.put(u,listu);
+            verticesSet.add(u);
+        } else {
+            listu.add(v);
         }
         E++;
     }
 
     public void removeEdge(int v, int u) {
-        List list = adjListMap.get(v);
-        if(null != list){
-            list.remove(u);
-            E--;
+        List listv = adjListMap.get(v);
+        if(null != listv){
+            listv.remove(listv.indexOf(u));
+            if(listv.size()==0){
+                adjListMap.remove(v);
+                verticesSet.remove(v);
+            }
+            //E--;
         }
+        List listu = adjListMap.get(u);
+        if(listu != null){
+            listu.remove(listu.indexOf(v));
+            if(listu.size()==0){
+                adjListMap.remove(u);
+                verticesSet.remove(u);
+            }
+        }
+        E--;
+    }
+
+    public List<Integer> getAdjacencyList(int v){
+        return adjListMap.get(v);
     }
 
     public int degree(int v) {
--- src/main/java/test/Test5.java
+++ src/main/java/test/Test5.java
@@ -4,8 +4,7 @@ import com.hr.Graph;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by rmandada on 28/1/15.
@@ -14,9 +13,51 @@ public class Test5 {
     public static void main(String[] args) throws FileNotFoundException {
         Graph graph = new Graph(200);
 
-        Scanner in = new Scanner(new File("/home/rmandada/Downloads/kargerMinCut.txt"));
+        Scanner in = new Scanner(new File("C:\\Users\\RANGA\\Desktop\\kargerMinCut.txt"));
         while (in.hasNext()){
-            System.out.println(in.nextLine());
+            //System.out.println(in.nextLine());
+            String[] str = in.nextLine().split("\t");
+            //int[] ints = new int[str.length];
+            int v = Integer.parseInt(str[0]);
+            //System.out.println(str.length);
+            for (int i = 1; i <str.length ; i++) {
+                //ints[i] = Integer.parseInt(str[i]);
+                graph.addEdge(v, Integer.parseInt(str[i]));
+            }
         }
+        int noOfV = graph.getV();
+        while(noOfV >2){
+            Random random = new Random();
+            Set<Integer> s = graph.getVerticesSet();
+            int v = new ArrayList<Integer>(s).get(random.nextInt(s.size()));
+            List<Integer> listv = graph.getAdjacencyList(v);
+            int u = listv.get(random.nextInt(listv.size()));
+            List<Integer> listu = graph.getAdjacencyList(u);
+            graph.removeEdge(v,u);
+
+            int s1 = listv.size();
+            int s2 = listu.size();
+            if(s1>=s2){
+                for(int i : listu){
+                    if(!listv.contains(i)){
+                        graph.addEdge(v,i);
+                    }
+                }
+                graph.getAdjListMap().remove(u);
+            } else {
+                for (int i: listv){
+                    if(!listu.contains(i)){
+                        graph.addEdge(u,i);
+                    }
+                }
+                graph.getAdjListMap().remove(v);
+            }
+            noOfV--;
+        }
+
+
+
+
+        System.out.println(graph.getAdjListMap());
     }
 }
